001ad59fd59ca390daa47dbfc1178069b348b153
Karl Knechtel <zahlman@proton.me>
2024-06-26 16:46:40 -0400

    Use a config file to describe the file format
    The format is defined in a TOML file. For now, it looks like:
    
    [input_format]
    columns = 16
    rows = 16
    tile_width = 8
    tile_height = 8
    bpp = -1

--- src/retile.py
+++ src/retile.py
@@ -1,4 +1,6 @@
 import sys
+from tomllib import load as load_toml
+from types import SimpleNamespace as Namespace
 
 import imageio as iio
 import numpy as np
@@ -17,11 +19,27 @@ def _unpack_mask(bpp):
     return (1 << abs(bpp)) - 1
 
 
-def _default_palette(bpp):
-    return _byte_values[::255//_unpack_mask(bpp)]
+def _default_palette(config):
+    return _byte_values[::255//_unpack_mask(config.input_format.bpp)]
 
 
-def load_raw(filename, *, c, r, tw, th, bpp):
+def _make_namespace(obj):
+    if not isinstance(obj, dict):
+        return obj
+    return Namespace(**{
+        k: _make_namespace(v)
+        for k, v in obj.items()
+    })
+
+
+def _get_config(config_file):
+    with open(config_file, 'rb') as f: # why does tomllib expect binary?
+        return _make_namespace(load_toml(f))
+
+
+def load_raw(filename, fmt):
+    r, c, th, tw = fmt.rows, fmt.columns, fmt.tile_height, fmt.tile_width
+    bpp = fmt.bpp
     data = np.fromfile(filename, dtype=np.uint8)
     # compute all the shifted, masked and scaled results for each byte
     # along a second dimension, then ravel it back into 1d data.
@@ -35,8 +53,9 @@ def load_raw(filename, *, c, r, tw, th, bpp):
     return unpacked.transpose((0, 2, 1, 3)).reshape((r*th, c*tw))
 
 
-def raw_to_png(filename, *, c, r, tw, th, bpp):
-    raw = load_raw(filename, c=c, r=r, tw=tw, th=th, bpp=bpp)
-    result = _default_palette(bpp)[raw]
-    out = filename.removesuffix('.bin') + '.png'
+def raw_to_png(image_filename, config_filename):
+    config = _get_config(config_filename)
+    raw = load_raw(image_filename, config.input_format)
+    result = _default_palette(config)[raw]
+    out = image_filename.removesuffix('.bin') + '.png'
     iio.imwrite(out, result)
