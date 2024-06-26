282d2f3a21a329ecfdbf8bd6133ecfbfabb5aabd
Karl Knechtel <zahlman@proton.me>
2024-06-26 15:40:25 -0400

    Refactoring
    * Shift array is now computed as a slice of a common array of ascending
      np.uint8 values. This allows the dtype to be specified only once.
      While debugging this, apparently fixed an existing bug (shifts for
      bpp of -2 and -4 should be 6 and 4 respectively, not 7).
    
    * Instead of scaling the data by a constant factor, the concept of a
      "palette" is introduced - data values are mapped through the palette.
      (The default palette uses values in arithmetic sequence, for the same
      net result as before.)
    
    * The loading of the image data is now separated from palette mapping
      and output file conversion.

--- src/retile.py
+++ src/retile.py
@@ -4,28 +4,39 @@ import imageio as iio
 import numpy as np
 
 
+_byte_values = np.arange(0, 256, dtype=np.uint8)
+
+
 def _unpack_shift(bpp):
-    low = 0 if bpp > 0 else 7
-    high = 8 if bpp > 0 else -1
-    return np.arange(low, high, bpp, dtype=np.uint8)
+    low = 0 if bpp > 0 else 8 + bpp 
+    high = 8 if bpp > 0 else None 
+    return _byte_values[low:high:bpp]
 
 
 def _unpack_mask(bpp):
     return (1 << abs(bpp)) - 1
 
 
-def raw_to_png(filename, *, c, r, tw, th, bpp):
+def _default_palette(bpp):
+    return _byte_values[::255//_unpack_mask(bpp)]
+
+
+def load_raw(filename, *, c, r, tw, th, bpp):
     data = np.fromfile(filename, dtype=np.uint8)
     # compute all the shifted, masked and scaled results for each byte
     # along a second dimension, then ravel it back into 1d data.
     shift, mask = _unpack_shift(bpp), _unpack_mask(bpp)
     # 1bpp -> 255, 2bpp -> 85, 4bpp -> 17, 8bpp -> 1
-    factor = np.uint8(255 // mask)
-    data = (((data[..., None] >> shift) & mask) * factor).reshape(-1)
+    data = ((data[..., None] >> shift) & mask).reshape(-1)
     # Reinterpret as a grid of tiles which are each grids of pixels.
     unpacked = data.reshape(r, c, th, tw)
     # group vertical and horizontal dimensions, then flatten
     # adapted from https://stackoverflow.com/questions/9071446
-    result = unpacked.transpose((0, 2, 1, 3)).reshape((r*th, c*tw))
+    return unpacked.transpose((0, 2, 1, 3)).reshape((r*th, c*tw))
+
+
+def raw_to_png(filename, *, c, r, tw, th, bpp):
+    raw = load_raw(filename, c=c, r=r, tw=tw, th=th, bpp=bpp)
+    result = _default_palette(bpp)[raw]
     out = filename.removesuffix('.bin') + '.png'
     iio.imwrite(out, result)
