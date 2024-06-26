38cfbb83bc0a3e8abb260c28f3e586ce33a02912
Karl Knechtel <zahlman@proton.me>
2024-06-26 14:24:49 -0400

    Remove script interface
    The CLI for this program will need proper design later. For now, the
    functionality will just be tested by using the code as a library.
    
    Also made the conversion function use keyword-only arguments, because
    more will certainly be added.

--- src/retile.py
+++ src/retile.py
@@ -4,7 +4,7 @@ import imageio as iio
 import numpy as np
 
 
-def raw1bpp(filename, c, r, tw, th):
+def raw1bpp(filename, *, c, r, tw, th):
     data = np.fromfile(filename, dtype=np.uint8)
     unpacked = np.unpackbits(data).reshape(r, c, th, tw)
     # group vertical and horizontal dimensions, then flatten
@@ -12,8 +12,3 @@ def raw1bpp(filename, c, r, tw, th):
     result = unpacked.transpose((0, 2, 1, 3)).reshape((r*th, c*tw)) * 255
     out = filename.removesuffix('.bin') + '.png'
     iio.imwrite(out, result)
-
-
-if __name__ == '__main__':
-    filename, c, r, tw, th = sys.argv[1:]
-    raw1bpp(filename, int(c), int(r), int(tw), int(th))
