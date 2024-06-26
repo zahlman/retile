fb4a6bba185573bcd774290f173d8335f01028b3
Karl Knechtel <zahlman@proton.me>
2024-06-26 14:21:26 -0400

    Generalize dimensions
    Accept additional parameters for the number of columns and rows of
    tiles, and the width and height (in pixels) of tiles.

--- src/retile.py
+++ src/retile.py
@@ -4,16 +4,16 @@ import imageio as iio
 import numpy as np
 
 
-def raw1bpp(filename):
+def raw1bpp(filename, c, r, tw, th):
     data = np.fromfile(filename, dtype=np.uint8)
-    # 16 rows of 16 columns of 8x8 tiles
-    unpacked = np.unpackbits(data).reshape(16, 16, 8, 8)
+    unpacked = np.unpackbits(data).reshape(r, c, th, tw)
     # group vertical and horizontal dimensions, then flatten
     # adapted from https://stackoverflow.com/questions/9071446
-    result = unpacked.transpose((0, 2, 1, 3)).reshape((128, 128)) * 255
+    result = unpacked.transpose((0, 2, 1, 3)).reshape((r*th, c*tw)) * 255
     out = filename.removesuffix('.bin') + '.png'
     iio.imwrite(out, result)
 
 
 if __name__ == '__main__':
-    raw1bpp(sys.argv[1])
+    filename, c, r, tw, th = sys.argv[1:]
+    raw1bpp(filename, int(c), int(r), int(tw), int(th))
