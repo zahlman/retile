0228a15c40e14a0a6b3dd08dfe1e64982cfbf757
Karl Knechtel <zahlman@proton.me>
2024-06-27 10:01:06 -0400

    Re-implement tiling
    Added `get_tiles` and `arrange_tiles` functions which extract a grid of
    tiles from the input (trimming any specified padding), re-work them
    into a new grid 16 columns wide (for now), and make a new image from
    that tile grid (re-padding between and around tiles as specified).
    
    The `tiles.result.bg` config value specifies a constant bitmap value to
    use for padding the tiles in the output (and adding a border). Bitmap
    data from the input outside of the tiles is ignored - in particular, it
    is NOT checked against the bg value (by design).

--- src/retile.py
+++ src/retile.py
@@ -46,9 +46,50 @@ def load_raw(filename, raw_format):
     return ((data[..., None] >> shift) & mask).reshape(-1, width)
 
 
+def _ensure(data, min_height, min_width):
+    height, width = data.shape
+    height_padding = min(0, min_height - height)
+    width_padding = min(0, min_width - width)
+    return np.pad(data, ((0, height_padding), (0, width_padding)))
+
+
+def get_tiles(data, tile_config):
+    x, y = tile_config.source.offset
+    pw, ph = tile_config.source.padding
+    tw, th = tile_config.size
+    c, r = tile_config.source.count
+    w, h = c * (tw + pw), r * (th + ph)
+    # Extract the region where the tiles are.
+    data = _ensure(data, y+h, x+w)[y:y+h, x:x+w]
+    # Rearrange into tiles (https://stackoverflow.com/questions/9071446)
+    data = data.reshape(r, th+ph, c, tw+pw).transpose((0, 2, 1, 3))
+    # Trim padding and produce a 1d array of tiles
+    return data[:, :, :th, :tw].reshape(-1, th, tw)
+
+
+def arrange_tiles(tiles, tile_config):
+    x, y = tile_config.result.offset
+    pw, ph = tile_config.result.padding
+    tw, th = tile_config.size
+    ew, eh = tile_config.result.extra
+    bg = tile_config.result.bg
+    # add per-tile padding on right and bottom
+    tiles = np.pad(tiles, ((0, 0), (0, ph), (0, pw)), constant_values=bg)
+    # Rearrange into 16-wide tile grid, then into bitmap
+    c = 16
+    r, extra = divmod(len(tiles), c)
+    assert not extra # tile count not divisible
+    tiles = tiles.reshape(r, c, th+ph, tw+pw).transpose((0, 2, 1, 3))
+    # Flatten to 2d and fix padding
+    result = tiles.reshape(r * (th+ph), c * (tw+pw))
+    # FIXME: does this work with negative values?
+    return np.pad(result, ((y, eh), (x, ew)), constant_values=bg)
+
+
 def raw_to_png(image_filename, config_filename):
     config = _get_config(config_filename)
     raw = load_raw(image_filename, config.format)
     result = _default_palette(config.format.input_bpp)[raw]
+    result = arrange_tiles(get_tiles(result, config.tiles), config.tiles)
     base, _, _ = image_filename.rpartition('.')
     iio.imwrite(f'{base}.{config.format.output_extension}', result)
